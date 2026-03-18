b94055d02b4f633fa8338240255ff7844ef844be
SiyuChen-2025 <230613202@mail.dhu.edu.cn>
2026-03-18 12:02:01 +0800

    switch deployment back to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 线上地址：https://siyuchen-2025.github.io/CHENSIYU-sProfile/
+
   base: '/CHENSIYU-sProfile/',
 })
